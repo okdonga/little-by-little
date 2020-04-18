d1ed1e9abc32e7350215b9454ef7f732329cf538
dongyoon-ok <okdonga@gmail.com>
2020-04-18 18:43:13 +0900

    Add logic to convert csv to geojson

--- scripts/csvToGeojson.js
+++ scripts/csvToGeojson.js
@@ -1,33 +1,65 @@
-console.log("hello");
-const csvToJson = require("csvtojson");
-const fs = require("fs");
+const csvToJson = require('csvtojson');
+const fs = require('fs')
+const filename = 'time_series_covid19_confirmed_global';
+
+let dateRanges = []; 
+
+function testing(features, date, count, row) {
+    let params = {
+        "type": "Feature",
+        "properties": {
+            "country_region": row["Country/Region"],
+            "province_state": row["Province/State"],
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [row['Long'], row['Lat']]
+        }
+    }
+
+    params['properties']["Date"] = date;
+    params['properties']["Confirmed"] = count
+
+    return params; 
+}
+
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+  
+const features = []
 
 const processFile = async () => {
-  const rawData = await csvToJson({
-    trim: true
-  }).fromFile("03-17-2020.csv");
+    const rawData = await csvToJson({
+        trim: true,
+    })
+    .on('header',(header)=>{
+        header.forEach((val, idx) => {
+            if (idx > 10) {
+                dateRanges.push(val);
+            }   
+        })
+    }).fromFile(`./COVID-19/csse_covid_19_data/csse_covid_19_time_series/${filename}.csv`);
+  
+    await asyncForEach(rawData, async(row) => {
+        await asyncForEach(dateRanges, async (date) => {
+            try {
+              const count = +row[date]
+              if (count > 0) {
+                const res = await testing(features,  date, count, row)
+                features.push({...res})
+              }
+            } catch(err) {
+                console.log(err)
+            }
+        })       
+    });
 
-//   const features = [];
-//   rawData.forEach(row => {
-//     let feature = {
-//       type: "Feature",
-//       properties: {
-//         Confirmed: row["Confirmed"],
-//         Deaths: row["Deaths"],
-//         Recovered: row["Recovered"],
-//         Date: "2020-03-17"
-//       },
-//       geometry: {
-//         type: "Point",
-//         coordinates: [row["Longitude"], row["Latitude"]]
-//       }
-//     };
-
-//     features.push(feature);
-//   });
-
-//   const geojsonOutput = { type: "FeatureCollection", features };
-//   fs.writeFileSync("03-17-2020.geojson", JSON.stringify(geojsonOutput), "utf8");
+    const geojsonOutput = { "type": "FeatureCollection", features }
+    fs.writeFileSync(`public/data/${filename}.geojson`, JSON.stringify(geojsonOutput), 'utf8')
 };
 
-processFile();
+processFile()
+
