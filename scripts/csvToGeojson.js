613e2cca8e0a8eca5ee39f18f77d04481f22594b
dongyoon-ok <okdonga@gmail.com>
2020-04-25 22:11:23 +0900

    Clean up

--- scripts/csvToGeojson.js
+++ scripts/csvToGeojson.js
@@ -1,14 +1,55 @@
 const csvToJson = require('csvtojson');
 const fs = require('fs')
 const input = process.argv[2].toLocaleLowerCase();
-const isValidInput = ['confirmed','deaths','recovered'];
+
+const LAYER_TYPE = {
+    DAILY: "daily",
+    CONFIRMED: "confiremd", 
+    DEATHS: "deaths",
+    RECOVERED: "recovered",
+}
+
+const isValidInput = [LAYER_TYPE.CONFIRMED,LAYER_TYPE.DEATHS,LAYER_TYPE.RECOVERED];
+
 if (isValidInput.indexOf(input) < 0) throw "Parameter must be either confiremd, deaths, or recovered";
 
-const filename = `time_series_covid19_${input}_global`;
+function fileInfo(input) {
+    switch(input) {
+        case LAYER_TYPE.DAILY: 
+            return {
+                in: '',
+                out: ''
+            }
+        default: 
+            return {
+                in: `./COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_${input}_global.csv`,
+                out: `public/data/time_series_covid19_${input}_global.geojson`
+            }
+    }
+}
 
-let dateRanges = []; 
+function getGeojsonProperties(layer) {
+    switch(layer) {
+        case LAYER_TYPE.DAILY: 
+            return {
+                COUNTRY: "Country_Region",
+                PROVINCE: "Province_State",
+                LONG:  "Long_", 
+                LAT: "Lat",
+                DATE: "Date",
+            }
+        default: 
+            return {
+                COUNTRY: "Country/Region",
+                PROVINCE: "Province/State",
+                LONG:  "Long", 
+                LAT: "Lat",
+                DATE: "Date",
+            }
+    }
+}
 
-function testing(features, date, count, row) {
+function makeGeoJson(features, date, count, row) {
     let params = {
         "type": "Feature",
         "properties": {
@@ -33,9 +74,10 @@ async function asyncForEach(array, callback) {
   }
 }
   
-const features = []
-
 const processFile = async () => {
+    const filename = fileInfo(input);
+    let dateRanges = []; 
+
     const rawData = await csvToJson({
         trim: true,
     })
@@ -45,14 +87,14 @@ const processFile = async () => {
                 dateRanges.push(val);
             }   
         })
-    }).fromFile(`./COVID-19/csse_covid_19_data/csse_covid_19_time_series/${filename}.csv`);
-  
+    }).fromFile(filename.in);
+    const features = []  
     await asyncForEach(rawData, async(row) => {
         await asyncForEach(dateRanges, async (date) => {
             try {
               const count = +row[date]
               if (count > 0) {
-                const res = await testing(features,  date, count, row)
+                const res = await makeGeoJson(features,  date, count, row)
                 features.push({...res})
               }
             } catch(err) {
@@ -62,8 +104,7 @@ const processFile = async () => {
     });
 
     const geojsonOutput = { "type": "FeatureCollection", features }
-    fs.writeFileSync(`public/data/${filename}.geojson`, JSON.stringify(geojsonOutput), 'utf8')
+    fs.writeFileSync(filename.out, JSON.stringify(geojsonOutput), 'utf8')
 };
 
-processFile()
-
+processFile();
