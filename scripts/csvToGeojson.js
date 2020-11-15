d1b99d45ba3b1479f956a6ce82fadc0e51b1d7a2
dongyoon-ok <dongyoon.ok@linecorp.com>
2020-11-16 00:08:59 +0900

    Rearrange geojson data
    * china, australia, canada => add all the province data
    * All the depencie of 'Netherlands', 'United Kingdom', 'France', 'Denmark' => rearrange as separate country

--- scripts/csvToGeojson.js
+++ scripts/csvToGeojson.js
@@ -2,7 +2,22 @@ const fs = require('fs')
 const csvToJson = require('csvtojson');
 const { LAYER_TYPE } = require('./common');
 
-
+const mergeCountryList = ['Australia', 'Canada', 'China'];
+const splitProvinceList = ['Netherlands', 'United Kingdom', 'France', 'Denmark'];
+const fixLatLng = {
+    'China': { // Gansu
+        lat: 35.7518,
+        lng: 104.2861
+    },
+    'Australia': { // South Australia
+        lat: -34.9285,
+        lng: 138.6007
+    },
+    'Canada': { // Saskatchewan
+        lat: 52.9399,
+        lng: -106.4509
+    }
+}
 
 function fileInfo(layer) {
     switch(layer) {
@@ -17,6 +32,57 @@ function fileInfo(layer) {
                 out: `public/data/time_series_covid19_${layer}_global.geojson`
             }
     }
+}
+
+function setGroupByCountryMap({ country, province, date, cases, prevCount, change, lat, lng}) {
+    return {
+        country, 
+        date,
+        cases,
+        prevCount,
+        change,
+        lat, 
+        lng,
+    }
+}
+
+function updateGroupByCountryMap(groupByCountryMap, date, dateRanges, index, row) {
+    const country = row["Country/Region"];
+    const province = row["Province/State"];
+    const cases = +row[date]
+    const prevCount = +row[dateRanges[index-1]];
+    const change = count = prevCount;
+    const lat = fixLatLng[country] ? fixLatLng[country]['lat'] : row['Lat'];
+    const lng = fixLatLng[country] ? fixLatLng[country]['lng'] : row['Long'];
+    if (groupByCountryMap[country]) {
+        groupByCountryMap[country][date] = setGroupByCountryMap({ country, province, date, cases, prevCount, change, lat, lng })
+    } else {
+        groupByCountryMap[country] = {
+            [date]: setGroupByCountryMap({ country, province, date, cases, prevCount, change, lat, lng })
+            
+        }
+    }
+    
+}
+
+function XmakeGeoJson(layer, data) {
+    // console.log(layer)
+    const { country, date, cases, change, lat, lng } = data;
+    // console.log(data)
+    let params = {
+        "type": "Feature",
+        "properties": {
+            "country_region": country,
+            "Date": date,
+            [layer]: cases,
+            "change": change
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [lng, lat],
+        }
+    }
+    return params; 
 }
 
 function makeGeoJson(layer, date, count, row, change) {
@@ -57,12 +123,15 @@ async function generate(layer) {
             }   
         })
     }).fromFile(filename.in);
+    
+   
+    const groupByCountryMap = {};
+
     const features = []  
     await asyncForEach(rawData, async(row) => {
         if (layer === LAYER_TYPE.DAILY) {
             await asyncForEach(dateRanges, async (date, index) => {
                 try {
-                    
                   const count = +row[date];
                   const prevCount = +row[dateRanges[index-1]];
                   const change = count - prevCount;
@@ -91,20 +160,74 @@ async function generate(layer) {
             })   
         } else {
             await asyncForEach(dateRanges, async (date, index) => {
-                try {
-                  const count = +row[date]
-                  const prevCount = +row[dateRanges[index-1]];
-                  const change = count - prevCount;
-                  if (count > 0) {
-                    const res = await makeGeoJson(layer, date, count, row, change)
-                    features.push({...res})
-                  }
-                } catch(err) {
-                    console.log(err)
+                const country = row["Country/Region"];
+                const province = row["Province/State"];
+                const cases = +row[date]
+                const prevCount = +row[dateRanges[index-1]];
+                const change = count = prevCount;
+                const lat = fixLatLng[country] ? fixLatLng[country]['lat'] : row['Lat'];
+                const lng = fixLatLng[country] ? fixLatLng[country]['lng'] : row['Long'];
+
+                // group by country 
+                if (mergeCountryList.includes(country)) {
+                    //  Australia, Canada and China are reported at the province/state level.
+                    // fix lat lng point eg beijing for china 
+                    if (groupByCountryMap[country]) {
+                        if (groupByCountryMap[country][date]) {
+                            // plus 하는 경우 
+                            groupByCountryMap[country][date]['cases'] += count
+                        } else {
+                            // 날짜가 처음인 경우 
+                            groupByCountryMap[country][date] = setGroupByCountryMap({ country, province, date, cases, prevCount, change, lat, lng })
+                        }
+                    } else {
+                        groupByCountryMap[country] = {
+                            [date]: setGroupByCountryMap({ country, province, date, cases, prevCount, change, lat, lng })
+                            
+                        }
+                    }                    
+                } else if (splitProvinceList.includes(country)) {
+                    // Dependencies of the Netherlands, the UK, France and Denmark are listed under the province/state level. 
+                    // set province as country
+                    groupByCountryMap[province] = setGroupByCountryMap({ country, province, date, cases, prevCount, change, lat, lng })
+                    // groupByCountryMap[province] = {
+                    //     [date]: {
+                    //         country: province,
+                    //         date, 
+                    //         cases: count, 
+                    //         prevCount: +row[dateRanges[index-1]],
+                    //         change:  count - prevCount,
+                    //         lat: row['Lat'],
+                    //         lng: row['Long'],
+                    //     },
+                        
+                    // }
+                } else {
+                    updateGroupByCountryMap(groupByCountryMap, date, dateRanges, index, row)
                 }
+ 
+                // try {
+                //   const change = count - prevCount;
+                //   if (count > 0) {
+                //     const res = await makeGeoJson(layer, date, count, row, change)
+                //     features.push({...res})
+                //   }
+                // } catch(err) {
+                //     console.log(err)
+                // }
             })       
         }
     });
+
+    // console.log(groupByCountryMap)
+    Object.keys(groupByCountryMap).forEach(country => {
+        Object.values(groupByCountryMap[country]).forEach(dailyCases => {
+            if (dailyCases.cases > 0) {
+                const res = XmakeGeoJson(layer, dailyCases)
+                features.push({...res})
+            }
+        })
+    })
 
     const geojsonOutput = { "type": "FeatureCollection", features }
     fs.writeFileSync(filename.out, JSON.stringify(geojsonOutput), 'utf8')
